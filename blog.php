68227cc24b5cd5744420bc8c47870da029df5568
Afroza Sultana <mariamsultanamadeena@gmail.com>
2020-08-24 10:28:54 +0600

    blog php file added

---
+++ blog.php
@@ -0,0 +1,89 @@
+<!--blog side-->
+ <section class="sectiondr gb">
+            <div class="container">
+                <div class="section-title text-center">				
+                    <h3>Recent Educational Institutes</h3>
+                    <p>Education Directory providing resource on Education related information with an exclusive educational profile page featuring institute details including address, logo, phone/fax, location map and more. Search for the best educational institute from our Global Education Directory.</p>
+                </div><!-- end title -->
+
+                <div class="row">
+  
+                    <div class="col-lg-4 col-md-12">
+                        <div class="blog-box">
+                            <div class="image-wrap entry">		
+                            	<div class="directory-thumb">						
+                                <img src="pic/index2.jpg" alt="pic/images.jpg" class="img-responsive">
+                                <div class="magnifier">
+                                     <a href="#" title=""><i class="flaticon-add"></i></a>                             
+                                </div>
+                            </div><!-- end image-wrap -->
+</div>
+                            <div class="blog-desc">
+                                <h4><a href="#" title="">Deshtutor.com</a></h4>
+                                <p>Desh Tutor is a job site where on the one hand the parents can employ skilled teachers for their child, and on the other hand, experienced teachers can get part time job / tuition ... <br> 
+								<span class="green"><i class="fa fa-user"></i> Posted by : fokrul islam</span></p>
+                            </div><!-- end blog-desc -->
+
+                            <div class="post-meta">
+                                <ul class="list-inline">
+                                    <li><i class="fa fa-calendar"></i> 16-Dec-2018</li>                                    
+                                    <li class="fl-right"><a href="#" title="Deshtutor"><i class="fa fa-eye"></i> Views   1549 </a></li>
+                                </ul>
+                            </div><!-- end post-meta -->
+                        </div><!-- end blog -->
+                    </div><!-- end col -->
+ 
+                    <div class="col-lg-4 col-md-12">
+                        <div class="blog-box">
+                            <div class="image-wrap entry">		<div class="directory-thumb">						
+                                <img src="pic/NPPGURXBJ5HLPD7AMK5NRYKB2U.JPG" alt="William Carey International School" class="img-responsive">
+                                <div class="magnifier">
+                                     <a href="" title="William Carey International School"><i class="flaticon-add"></i></a>                             
+                                </div>
+                            </div><!-- end image-wrap -->
+</div>
+                            <div class="blog-desc">
+                                <h4><a href="#" title="William Carey International School">William Carey International School</a></h4>
+                                <p>The WCIS training of students must ensure every aspect of learning, including understanding and decision making. The WCIS motto of Pride, Unity and Excellence emphasizes building c... <br> 
+								<span class="green"><i class="fa fa-user"></i> Posted by : Samir Boidya</span></p>
+                            </div><!-- end blog-desc -->
+
+                            <div class="post-meta">
+                                <ul class="list-inline">
+                                    <li><i class="fa fa-calendar"></i> 29-Oct-2018</li>                                    
+                                    <li class="fl-right"><a href="" title="William Carey International School"><i class="fa fa-eye"></i> Views   1079 </a></li>
+                                </ul>
+                            </div><!-- end post-meta -->
+                        </div><!-- end blog -->
+                    </div><!-- end col -->
+ 
+                    <div class="col-lg-4 col-md-12">
+                        <div class="blog-box">
+                            <div class="image-wrap entry">		<div class="directory-thumb">						
+                                <img src="pic/index2.jpg" alt="School of Light" class="img-responsive">
+                                <div class="magnifier">
+                                     <a href="" title="School of Light"><i class="flaticon-add"></i></a>                             
+                                </div>
+                            </div><!-- end image-wrap -->
+</div>
+                            <div class="blog-desc">
+                                <h4><a href="#" title="School of Light">School of Light</a></h4>
+                                <p>To begin with a quality primary school (nursery through Class V) in the village of Senerganti Mission that will be a model school for all, whether it be secular or religious.
+To d... <br> 
+								<span class="green"><i class="fa fa-user"></i> Posted by : Rathin Mazumder</span></p>
+                            </div><!-- end blog-desc -->
+
+                            <div class="post-meta">
+                                <ul class="list-inline">
+                                    <li><i class="fa fa-calendar"></i> 04-Oct-2018</li>                                    
+                                    <li class="fl-right"><a href="#" title="School of Light"><i class="fa fa-eye"></i> Views   995 </a></li>
+                                </ul>
+                            </div><!-- end post-meta -->
+                        </div><!-- end blog -->
+                    </div><!-- end col -->
+ 
+<hr class="invis">
+                </div><!-- end row -->				
+				
+            </div><!-- end container -->
+        </section>
