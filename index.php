05305c5d7df14f0148f3b321849a765aa83bcf5c
Afroza Sultana <mariamsultanamadeena@gmail.com>
2020-08-24 10:33:42 +0600

    index php file added

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+include("includes/header.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Homepage</title>
+	 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="css/bootstrap.min.css">
+      <link rel="stylesheet" href="css/main.css">
+      <link rel="stylesheet" href="css/footer.css">
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/th.css">
+
+
+</head>
+<body>
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="pic/0b4d3da40646462f99177b0d7db4697f.jpg" class="d-block w-100" alt="pic/0b4d3da40646462f99177b0d7db4697f.jpg" height="650" width="1300">  </div>
+    <div class="carousel-item">
+      <img src="pic/34407924_957151117800574_8790046670327906304_n.jpg" class="d-block w-100" alt="pic/0b4d3da40646462f99177b0d7db4697f.jpg" height="650" width="1300">
+    </div>
+    <div class="carousel-item">
+      <img src="pic/index3.jpg" class="d-block w-100" alt="pic/0b4d3da40646462f99177b0d7db4697f.jpg" height="650" width="1300">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+     </div>
+</div>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity=" sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>      
+      <script src="js/fonticon.js"></script></body>
+</html>
+<?php
+ include("blog.php");
+?>
+<?php
+ include("includes/footer.php");
+?>
