503ff8431502ba100c50632bd46fb18046e4b6dc
Israt-jahan1212 <70111431+Israt-jahan1212@users.noreply.github.com>
2020-09-01 02:34:16 +0600

    Add files via upload

---
+++ registerpage.php
@@ -0,0 +1,41 @@
+<?php
+include("includes/conection.php"); ?>
+<?php
+include("includes/header.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign In cover page</title>
+		 <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="css/bootstrap.min.css">
+	   <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/th.css">
+	  <style type="text/css">
+	  	#imagecover{
+        background-image: url("pic/Cool-3d-Abstract-Tile-Squares-Background-Blue.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+      )
+     }
+ </style>
+      </head>
+<body>
+	<section>
+	     <div id="imagecover">
+	     	<h2>SIGN IN PAGE</h2>
+		   <div class="center"><a href="tutor-signup.php"><button type="button" class="btn btn-dark btn-lg">As a Tutor</button></a>
+			<a href="#"><button type="button" class="btn btn-dark btn-lg">As a Student</button></a></div>
+	     </div>
+	</section>
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
+
+<?php
+ include("blog.php");
+
+ include("includes/footer.php");
+?>
